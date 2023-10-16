fc4ec2c889eeb61c371c876a2be6308360313030
ahsanwebengr <ahsanwebengr@gmail.com>
2023-10-16 22:17:53 +0500

    update navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,18 @@
-// Responive NAvbar 
+// Responsive Navbar 
 const icon = document.querySelector('#icon');
-// const toogleBtn = document.querySelector('.menu-bar');
 const navMenu = document.querySelector('.nav-menu');
 
 icon.addEventListener('click', () => {
     navMenu.classList.toggle('show');
-    icon.classList.toggle('fa-times')
+    icon.classList.toggle('fa-times');
 });
 
-// On scroll Fuction 
+// On scroll Function
 
 const navbar = document.querySelector('.navbar');
 
 window.onscroll = function () {
-    if(document.documentElement.scrollTop > 20){
-        navbar.classList.add('onScroll');
-    }
-    else{
-        navbar.classList.remove('onScroll');
-    }
+    document.documentElement.scrollTop > 20 ? navbar.classList.add('onScroll') : navbar.classList.remove('onScroll');
 }
 
 // Image Source change 
@@ -27,17 +21,17 @@ const mainImg = document.querySelector('#mainImg');
 const smallImg = document.querySelectorAll('.small-img');
 
 smallImg[0].onclick = function () {
-    mainImg.src = smallImg[0].src ;
+    mainImg.src = smallImg[0].src;
 };
 
 smallImg[1].onclick = function () {
-    mainImg.src = smallImg[1].src ;
+    mainImg.src = smallImg[1].src;
 }
 
 smallImg[2].onclick = function () {
-    mainImg.src = smallImg[2].src ;
+    mainImg.src = smallImg[2].src;
 }
 
 smallImg[3].onclick = function () {
-    mainImg.src = smallImg[3].src ;
+    mainImg.src = smallImg[3].src;
 }
